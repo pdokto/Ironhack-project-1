f665c34b5f89b799d4f3d9cdb98118a1d15150b2
Patrick Doktorowski <patrick.doktorowski@googlemail.com>
2022-01-25 15:57:07 +0100

    finish player movement animation, deleted unused sprites

--- js/game.js
+++ js/game.js
@@ -14,18 +14,15 @@ class Game {
         this.playerImageIdle=[]
         this.playerImageWalkDown=[]
         this.playerImageWalkUp=[]
+        this.playerImageWalkLeft =[]
+        this.playerImageWalkRight =[]
     }
     preload(){
         this.backgroundImage =[
             {src: loadImage('assets/background/map.png')}
-        
         ]
-        this.playerImage= loadImage('assets/player/_down_idle1.png')
 
-        this.playerImageIdle[0]=loadImage('assets/player/_down_idle1.png')
-        this.playerImageIdle[1]=loadImage('assets/player/_down_idle2.png')
-        this.playerImageIdle[2]=loadImage('assets/player/_down_idle3.png')
-        this.playerImageIdle[3]=loadImage('assets/player/_down_idle4.png')
+        this.playerImage= loadImage('assets/player/_down_walk1.png')
 
         this.playerImageWalkDown[0]=loadImage('assets/player/_down_walk1.png')
         this.playerImageWalkDown[1]=loadImage('assets/player/_down_walk2.png')
@@ -33,6 +30,28 @@ class Game {
         this.playerImageWalkDown[3]=loadImage('assets/player/_down_walk4.png')
         this.playerImageWalkDown[4]=loadImage('assets/player/_down_walk5.png')
 
+        this.playerImageWalkLeft[0]=loadImage('assets/player/_side_walk_left1.png')
+        this.playerImageWalkLeft[1]=loadImage('assets/player/_side_walk_left2.png')
+        this.playerImageWalkLeft[2]=loadImage('assets/player/_side_walk_left3.png')
+        this.playerImageWalkLeft[3]=loadImage('assets/player/_side_walk_left4.png')
+        this.playerImageWalkLeft[4]=loadImage('assets/player/_side_walk_left5.png')
+        this.playerImageWalkLeft[5]=loadImage('assets/player/_side_walk_left6.png')
+
+        this.playerImageWalkRight[0]=loadImage('assets/player/_side_walk_right1.png')
+        this.playerImageWalkRight[1]=loadImage('assets/player/_side_walk_right2.png')
+        this.playerImageWalkRight[2]=loadImage('assets/player/_side_walk_right3.png')
+        this.playerImageWalkRight[3]=loadImage('assets/player/_side_walk_right4.png')
+        this.playerImageWalkRight[4]=loadImage('assets/player/_side_walk_right5.png')
+        this.playerImageWalkRight[5]=loadImage('assets/player/_side_walk_right6.png')
+        
+        this.playerImageWalkUp[0]=loadImage('assets/player/_up_walk1.png')
+        this.playerImageWalkUp[1]=loadImage('assets/player/_up_walk2.png')
+        this.playerImageWalkUp[2]=loadImage('assets/player/_up_walk3.png')
+        this.playerImageWalkUp[3]=loadImage('assets/player/_up_walk4.png')
+        this.playerImageWalkUp[4]=loadImage('assets/player/_up_walk5.png')
+        this.playerImageWalkUp[5]=loadImage('assets/player/_up_walk6.png')
+        
+        
         
         this.fishImage= loadImage('assets/fish/fishA.png')
     }
--- js/player.js
+++ js/player.js
@@ -7,6 +7,7 @@ class Player {
         this.speed =2
         this.facing = 'down'
         this.i=0
+        
 
         this.inventory = [
                             {name: 'fish', imgSRC:'this.fishImage', amount: 0, posX: 0 , posY:0 , imgHeight:0 ,imgWidth:0},
@@ -20,10 +21,9 @@ class Player {
         if(keyIsDown(83) && this.y <=(450-this.height) && game.fishingState===false){
             this.y +=this.speed
             this.facing='down'
-            if (frameCount %10 ==0){ //modulo 60 >> to it every one second
             
-            
-                game.playerImage = game.playerImageWalkDown[this.i%4]
+            if (frameCount %5 ==0){ //modulo 60 >> to it every one second
+                game.playerImage = game.playerImageWalkDown[this.i%game.playerImageWalkDown.length]
                 this.i++
             }
 
@@ -33,29 +33,42 @@ class Player {
         if(keyIsDown(87) && this.y >=0 && game.fishingState===false){
             this.y -=this.speed
             this.facing= 'up'
+
+            if (frameCount %5 ==0){ //modulo 60 >> to it every one second
+                game.playerImage = game.playerImageWalkUp[this.i%game.playerImageWalkUp.length]
+                this.i++
+            }
+            
         }
 
         //moveleft
         if (keyIsDown(65) && this.x >=0 && game.fishingState===false){
             this.x -=this.speed
             this.facing='left'
+            
+            if (frameCount %5 ==0){ //modulo 60 >> to it every one second
+                game.playerImage = game.playerImageWalkLeft[this.i%game.playerImageWalkLeft.length]
+                this.i++
+            }
         }
 
         //moveright
         if(keyIsDown(68) && this.x <=(800-this.width) && game.fishingState===false){
             this.x +=this.speed
             this.facing='right'
+            
+            if (frameCount %5 ==0){ //modulo 60 >> to it every one second
+                game.playerImage = game.playerImageWalkRight[this.i%game.playerImageWalkRight.length]
+                this.i++
+            }
+        
         }
+        
         image(game.playerImage, this.x, this.y, this.width, this.height)
         
-        // if (frameCount %30 ==0){ //modulo 60 >> to it every one second
-            
-            
-        //     game.playerImage = game.playerImageIdle[this.i%4]
-        //     this.i++
-        // }
+       
 
-        if(this.x >500 && keyIsDown(69)){
+        if(this.x >600 && keyIsDown(69)){
             game.fishingState=true
             game.fishing.hookBarPosition = 275  //set these to default again
             game.fishing.progress=-200
